6eb019daa00fc7702de8138fc9ca4ffcd6233f79
Sumit Sasidharan <sumit.onorall@gmail.com>
2022-05-11 12:24:28 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const WebSocket = require('ws');
 const server = new WebSocket.Server({port: '8080'});
+
+// default 'connection'
+server.on('connection', (socket) => {
+
+    // listen to incoming messages, 'message' event
+    socket.on('message', message => {
+        console.log(message)
+
+        // send a message back to the client
+        socket.send(`Okay Roger that! ${message}`);
+    })
+
+})
